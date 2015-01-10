2005e2b91ac115e99ff8651638d54dac467029dc
Francesco Pezzella <franzpezzella@gmail.com>
2015-01-10 12:45:42 +0100

    Add more useful stuff.

--- brew.sh
+++ brew.sh
@@ -16,23 +16,28 @@ brew upgrade
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+# Avoid --default-names flag as this wipes out native OS X bins. Prefer aliasing.
 brew install coreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
-
 # Install some other useful utilities like `sponge`.
 brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed
+brew install gnu-tar
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
 brew install bash-completion
 
+# Install more GNU stuff
+
 # Install `wget` with IRI support.
 brew install wget --with-iri
+
+# generic colouriser  http://kassiopeia.juls.savba.sk/~garabik/software/grc/
+brew install grc
 
 # Install RingoJS and Narwhal.
 # Note that the order in which these are installed is important;
@@ -42,30 +47,40 @@ brew install narwhal
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
+# Don't override here, use aliases instead and keep normal vim
+# MacVim fullscreen mode is messed up and doesn't work well in Yosemite.
+brew install macvim
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/screen
-brew install homebrew/php/php55 --with-gmp
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
 brew install bfg
 brew install binutils
 brew install binwalk
-brew install caskroom/cask/brew-cask
 brew install cifer
+brew install cmake
 brew install colordiff
+brew install curl
 brew install dex2jar
 brew install dns2tcp
 brew install fcrackzip
 brew install foremost
+brew install gcc
 brew install hashpump
 brew install hydra
 brew install john
 brew install knock
+brew install libxml2
+brew install libxslt
+
 brew install nmap
 brew install ngrep
+brew install openssl
 brew install pngcheck
 brew install socat
+brew install ssh-copy-id
 brew install sqlmap
+brew install tmux
 brew install tcpflow
 brew install tcpreplay
 brew install tcptrace
@@ -76,11 +91,13 @@ brew install xz
 # Install other useful binaries.
 brew install ack
 #brew install exiv2
+brew install ffmpeg --with-libvpx
 brew install git
 brew install imagemagick --with-webp
 brew install lua
 brew install lynx
 brew install p7zip
+brew install pcre
 brew install pigz
 brew install pv
 brew install rename
@@ -90,9 +107,11 @@ brew install tree
 brew install webkit2png
 brew install zopfli
 
-# Install Node.js. Note: this installs `npm` too, using the recommended
-# installation method.
-brew install node
+echo "# use GNU utils" >> ~/.path
+echo "export PATH=\"$(brew --prefix coreutils)/libexec/gnubin:$PATH\"" >> ~/.path
+# except for ls... http://stackoverflow.com/questions/26585868/gnu-ls-from-coreutils-missing-os-x-acl-implementation
+mv /usr/local/opt/coreutils/libexec/gnubin/ls /usr/local/opt/coreutils/libexec/gnubin/ls_backup
+ln -s /bin/ls /usr/local/opt/coreutils/libexec/gnubin/ls
 
 # Remove outdated versions from the cellar.
 brew cleanup
