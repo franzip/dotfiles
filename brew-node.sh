a82e59e85861b2a6d55c9684af1f1f306a71fd90
Francesco Pezzella <franzpezzella@gmail.com>
2015-01-10 12:44:19 +0100

    Update stuff before install.

--- brew-node.sh
+++ brew-node.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+npm install node-gyp -g
+npm install npm -g
+npm update -g
+
 npm install -g grunt-cli bower yo gulp express
 # Chrome terminal dependencies
 npm install -g socket.io devtools-terminal
