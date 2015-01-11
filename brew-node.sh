c964888cd1b7db441c05c72bb00a11f1b9e41aca
Francesco Pezzella <franzpezzella@gmail.com>
2015-01-11 02:31:06 +0100

    Don't install dev-terminal deps since they are broken right now (pty.js)

--- brew-node.sh
+++ brew-node.sh
@@ -5,7 +5,5 @@ npm install npm -g
 npm update -g
 
 npm install -g grunt-cli bower yo gulp express
-# Chrome terminal dependencies
-npm install -g socket.io devtools-terminal
 # ST3 plugins
 npm install -g coffee-script jshint less csslint
