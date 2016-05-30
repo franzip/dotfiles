c3f08de2e5a2513f0ed3b778c0c8f5399c61936a
Francesco Pezzella <franzpezzella@gmail.com>
2016-05-30 11:27:41 +0200

    Remove stuff

--- brew-node.sh
+++ brew-node.sh
@@ -5,33 +5,74 @@ npm install node-gyp -g
 npm install npm -g
 npm update -g
 
-# basic
-npm install -g grunt-cli grunt bower gulp express express-generator jasmine karma mocha browserify
-
-# ST3 deps
-npm install -g coffee-script jshint jsxhint eslint less csslint typescript sasslint scsslint coffeelint
-
-# stuff
-npm install -g backbone nodemon async socket.io mongoose hapi request forever mkdirp
-
-npm install -g cordova phonegap ionic
-
-npm install -g lodash loopback pm2 debug commander moment q passport redis knockout jsx
-
-npm install -g nodemailer minimist chalk colors underscore jade bluebird sails
-
-npm install -g mean-cli ember-cli
-
-npm install -g phantomjs prettyjson node-inspector
-
-npm install -g vue vue-cli webpack jsx react babel babel-cli
+# Basic
+npm install -g grunt
+npm install -g bower
+npm install -g gulp
+npm install -g grunt-cli
+npm install -g browserify
+# Languages
+npm install -g less
+npm install -g coffee-script
+npm install -g typescript
+npm install -g jsx
+npm install -g elm
+# Linters
+npm install -g jshint
+npm install -g eslint
+npm install -g jscs
+npm install -g jsxhint
+npm install -g eslint
+npm install -g coffeelint
+npm install -g csslint
+npm install -g sasslint
+npm install -g scsslint
+# Webpack
+npm install -g webpack
+npm install -g webpack-dev-server
+# Babel
+npm install -g babel
+npm install -g babel-cli
+# CLI
+npm install -g mean-cli
+npm install -g ember-cli
+npm install -g vue-cli
+npm install -g resume-cli
+# Frameworks
+npm install -g cordova
+npm install -g phonegap
+npm install -g ionic
+npm install -g sails
+# Testing
+npm install -g jasmine
+npm install -g jasmine-node
+npm install -g karma
+npm install -g mocha
+# Utils
+npm install -g nodemon
+npm install -g forever
+npm install -g prettyjson
+npm install -g node-inspector
+npm install -g phantomjs
 
 # Meteor.js
 curl https://install.meteor.com/ | sh
 
-# yo
+# Generators
+npm install -g express-generator
 npm install -g yo
-npm install -g generator-angular generator-angular-fullstack generator-backbone
-npm install -g generator-karma generator-mocha generator-gulp-webapp generator-polymer
-npm install -g generator-node generator-bootstrap generator-jquery generator-mean
-npm install -g generator-webapp generator-meteor generator-backbone-mocha
+npm install -g generator-angular
+npm install -g generator-angular-fullstack
+npm install -g generator-backbone
+npm install -g generator-babel-boilerplate
+npm install -g generator-karma
+npm install -g generator-mocha
+npm install -g generator-gulp-webapp
+npm install -g generator-polymer
+npm install -g generator-node
+npm install -g generator-bootstrap
+npm install -g generator-jquery
+npm install -g generator-mean
+npm install -g generator-webapp
+npm install -g generator-meteor
+npm install -g generator-backbone-mocha
