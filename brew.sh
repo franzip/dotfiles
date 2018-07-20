eeb7be6647b339d1a83884de9b18d80f3c47b60e
Francesco Pezzella <franzpezzella@gmail.com>
2018-07-20 15:54:45 +0200

    bump

--- brew.sh
+++ brew.sh
@@ -1,43 +1,5 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
-
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# Install command line tools
-xcode-select --install
-
-
-# Get homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Get ready to brew
-brew update
-brew upgrade --all
-brew doctor
-
-echo "Installing Cask..."
-# Install Cask
-brew tap caskroom/cask
-
-echo "Tapping brew things."
-# tap all the things directly here
-brew tap caskroom/versions
-brew tap homebrew/dupes
-brew tap homebrew/python
-brew tap homebrew/apache
-brew tap homebrew/php
-brew tap homebrew/science
-
-# Get ready to brew
-brew update
-brew cleanup
-cask cleanup
-
 # for the c alias (syntax highlighted cat)
 sudo easy_install Pygments
 
@@ -66,9 +28,9 @@ brew install wget --with-iri
 # generic colouriser  http://kassiopeia.juls.savba.sk/~garabik/software/grc/
 brew install grc
 
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/screen
-brew install homebrew/dupes/openssh
+brew install grep
+brew install homebrew/core/screen
+brew install homebrew/core/openssh
 brew install gh
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
@@ -109,7 +71,7 @@ brew install tmux
 brew install tcpflow
 brew install tcpreplay
 brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
+brew install ucspi-tcp
 brew install xz
 
 # nginx
@@ -117,13 +79,13 @@ brew install nginx
 
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
+brew install exiv2
+brew install libvpx
 brew install ffmpeg --with-libvpx
 brew install git
 brew install git-flow
 brew install elixir
 brew install go
-brew install ngrok
 brew install imagemagick --with-webp
 brew install icu4c
 brew install lua
@@ -148,15 +110,12 @@ brew install tidy-html5
 brew install phantomjs
 
 # Markdown TOC generator
-curl -L https://github.com/ekalinin/github-markdown-toc.go/releases/download/0.5.0/gh-md-toc.darwin.amd64.tgz > gh-md-toc.tgz
-tar -xvzf gh-md-toc.tgz
-rm gh-md-toc.tgz
+wget https://raw.githubusercontent.com/ekalinin/github-markdown-toc/master/gh-md-toc
 chmod a+x gh-md-toc
 mv gh-md-toc /usr/local/bin/gh-md-toc
 
 echo "# use GNU utils" >> ~/.path
 echo "export PATH=\"$(brew --prefix coreutils)/libexec/gnubin:$PATH\"" >> ~/.path
-# except for ls... http://stackoverflow.com/questions/26585868/gnu-ls-from-coreutils-missing-os-x-acl-implementation
 mv /usr/local/opt/coreutils/libexec/gnubin/ls /usr/local/opt/coreutils/libexec/gnubin/ls_gnu
 ln -s /bin/ls /usr/local/opt/coreutils/libexec/gnubin/ls
 
