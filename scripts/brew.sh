cb4573edf8d062e0f6dc4a707f86eee10d4aa5de
Francesco Pezzella <franzpezzella@gmail.com>
2023-01-07 11:09:50 +0100

    tweak

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -29,3 +29,7 @@ brew tap golangci/tap
 brew tap heroku/brew
 brew tap mongodb/brew
 brew tap dbcli/tap
+
+sudo spctl --master-disable
+
+echo "Change settings in: System Settings > Privacy & Security > Security"
